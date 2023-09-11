153e90d6405fc63baadf92e057f29e1f88cb1c2f
Jay Hogan <jayhogan.dev@gmail.com>
2023-09-10 22:22:53 -0700

    fixed preferred games map

--- components/PreferenceItem.tsx
+++ components/PreferenceItem.tsx
@@ -9,19 +9,21 @@ interface PreferenceItemProps {
 export default function PreferenceItem({ user }: PreferenceItemProps) {
   return (
     <View style={tw`h-fit w-full p-3`}>
-      <Text style={tw`text-white`}>Likes to play games on the course</Text>
+      <Text style={tw`text-white`}>Do you play games on the course?</Text>
       <Text style={tw`text-white`}>
         {user.preferences.playsGames ? "Yes" : "No"}
       </Text>
       <Text style={tw`text-white`}>Preferred Games</Text>
-      <View style={tw`flex flex-col`}>
+      <View style={tw`flex flex-row flex-wrap gap-4 gap-y-2`}>
         {user.preferences.preferredGames?.map((game, index) => (
-          <Text
-            key={index}
-            style={tw`text-white`}
-          >
-            - {game}
-          </Text>
+          <View style={tw`px-6 py-2 bg-cyan-700 rounded-lg items-center`}>
+            <Text
+              key={index}
+              style={tw`text-white`}
+            >
+              {game}
+            </Text>
+          </View>
         ))}
       </View>
     </View>
