b4ac0d4e0de473bc0f1a360f9b95892eec8c3d89
Jay Hogan <jayhogan.dev@gmail.com>
2023-09-10 20:41:08 -0700

    dummy data added

--- app/(tabs)/preferences.tsx
+++ app/(tabs)/preferences.tsx
@@ -4,8 +4,10 @@ import tw from "twrnc";
 
 export default function PreferencesScreen() {
   return (
-    <View style={tw`flex-1 items-center justify-center`}>
+    <View style={tw`flex-1 items-start justify-start p-10`}>
       <Text style={tw`text-lg font-bold`}>Preferences</Text>
+      <Text style={tw`text-md`}>Wolf</Text>
+      <Text style={tw`text-md`}>Nassau</Text>
     </View>
   );
 }
